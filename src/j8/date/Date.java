6896e3353967fceb1030c782735d1ee593e068da
HAMZA AIT OUMGHAR <hamza.aitoumghar.3@gmail.com>
2020-04-21 20:44:12 +0000

    first commit

---
+++ src/j8/date/Date.java
@@ -0,0 +1,18 @@
+package j8.date;
+
+import java.time.LocalDate;
+
+public class Date {
+
+
+    public static void main(String[] args) {
+
+        //Converting a string to date and time
+        //1-via a predefined formatter :  ISO-8601 (yyyy-MM-dd)
+        LocalDate localDate = LocalDate.parse("2020-01-12");
+
+
+
+
+    }
+}
